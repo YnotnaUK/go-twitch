a7603f818aad3c0025d31cdd329e4ffa5cc4e168
Antony Lee <antony@antonylee.co.uk>
2024-03-08 09:43:25 +0000

    initial commit
    * start of wrapper around the whol twitch suite

---
+++ bot/bot.go
@@ -0,0 +1,44 @@
+package bot
+
+import (
+	"log"
+
+	"github.com/ynotnauk/go-twitch/chat"
+	"github.com/ynotnauk/go-twitch/entities"
+	"github.com/ynotnauk/go-twitch/interfaces"
+)
+
+type Bot struct {
+	chat *chat.Client
+}
+
+func (b *Bot) ChatJoin(channel string) error {
+	err := b.chat.Join(channel)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Bot) OnTwitchChatConnect(handler func(*entities.ChatConnectMessage)) error {
+	b.chat.OnConnect(handler)
+	return nil
+}
+
+func (b *Bot) Start() {
+	log.Println("Starting bot...")
+	b.chat.Start()
+}
+
+func New(authProvider interfaces.AuthProvider) (*Bot, error) {
+	// Create chat client
+	chat, err := chat.NewClient(authProvider)
+	if err != nil {
+		return nil, err
+	}
+	// Create bot
+	bot := &Bot{
+		chat: chat,
+	}
+	return bot, nil
+}
